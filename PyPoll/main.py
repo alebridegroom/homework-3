21b573e376259acee26b3f1701b96b4be0f81d23
Alejandra Bridegroom <alejandrabridegroom@Alejandras-MacBook-Pro.local>
2023-05-29 20:42:57 -0700

    complete python2 script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,29 +6,31 @@ with open(election_data) as csvfile:
     csv_header = next(csvreader)
     tot_votes = []
     candidates = []
-    
+    #making seperate lists for candidates and vote count
     for i in csvreader:
         tot_votes.append(i[0])
         candidates.append(i[2])
-    
+    #total amount of votes
     tote_votes = len(tot_votes)
- 
+    #unique values of the candidates row using set() and putting it in a list
     candidates_set = list(set(candidates))
     
-    
+    #getting the count for each candidate of votes via the references of the rows in the unique value list
     c_votes = candidates.count(candidates_set[0])
     d_votes = candidates.count(candidates_set[1])
     r_votes = candidates.count(candidates_set[2])
     vote_max = [c_votes,d_votes,r_votes]
+    #zipping the votes in ad dictionary in order to get the max amount
     zip_count = dict(zip(candidates_set,vote_max))
     
-    
+    #finding the percent amount of each candidate
     c_percent = round((c_votes/tote_votes)*100,3)
     d_percent = round((d_votes/tote_votes)*100,3)
     r_percent = round((r_votes/tote_votes)*100,3)
-    percent_max = [c_percent,d_percent, r_percent]
     
+    #finding the winnder using max() fuanction with the key being .get
     winner = max(zip_count, key=zip_count.get)
+    
     
     
     print("Election Results")
